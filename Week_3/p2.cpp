5e1a220a27e26b7651a83182374efa294bec0b50
deeptadevkota <deeptadevkota21@gmail.com>
2021-10-05 21:17:49 +0545

    commit

--- Week_3/p2.cpp
+++ Week_3/p2.cpp
@@ -1,32 +1,49 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int compute_items()
+void compute_items(char c, unordered_map<char, set<string>> &m, vector<bool> &is_computed, unordered_map<char, unordered_map<char, set<string>>> &items, unordered_set<char> &nter)
 {
 
+    items[c][c] = m[c];
+
+    for (auto j : m[c])
+    {
+
+        if (nter.find(j[1]) != nter.end())
+        {
+            if (is_computed[c] == false)
+                compute_items(j[1], m, is_computed, items, nter);
+
+            for (auto k : items[j[1]])
+            {
+                items[c][k.first] = k.second;
+            }
+        }
+        
+    }
+
+    is_computed[c] = true;
 }
-
-
 
 int main()
 {
-    int n_ter,n_nter;
+    int n_ter, n_nter;
     cin >> n_ter;
 
-    unordered_set<char> ter,nter;
-    char c; 
+    unordered_set<char> ter, nter;
+    char c;
 
-    for(int i=0;i<n_ter;i++)
-    {   
+    for (int i = 0; i < n_ter; i++)
+    {
         cin >> c;
         ter.insert(c);
     }
 
     cin >> n_nter;
 
-    for(int i=0;i<n_nter;i++)
-    {  
+    for (int i = 0; i < n_nter; i++)
+    {
         cin >> c;
         nter.insert(c);
     }
@@ -34,28 +51,33 @@ int main()
     int n_pro;
     cin >> n_pro;
 
-    vector<string> grammar(n_pro+1);
+    vector<string> grammar(n_pro + 1);
 
     for (int i = 1; i <= n_pro; i++)
     {
         cin >> grammar[i];
     }
 
-    
-
-    string start_symbol;
+    char start_symbol;
     cin >> start_symbol;
 
-    grammar[0]="Z="+start_symbol;
+
+    string str="Z=";
+    str.push_back(start_symbol);
+    // cout << "str: " << str << endl;
+    grammar[0] = str;
+    // return 0;
 
     // cout << "The augmented grammar is:" << endl;
 
     // for(int i=0;i<=n_pro;i++)
     //     cout << grammar[i] << endl;
 
+
+    
     unordered_map<char, set<string>> m;
 
-    for (int i = 0; i <=n_pro; i++)
+    for (int i = 0; i <= n_pro; i++)
     {
         string p = ".";
         for (int j = 2; j < grammar[i].size(); j++)
@@ -70,23 +92,34 @@ int main()
         }
         if (p != ".")
             m[grammar[i][0]].insert(p);
+        
+    }
+    
+
+
+    // for(auto i:m['Z'])
+    // {
+    //     cout << i << endl;
+    // }
+
+    vector<bool> is_computed(256,0);
+    unordered_map<char, unordered_map<char, set<string>>> items;
+
+    compute_items('Z', m, is_computed, items, nter);
+
+    cout << endl;
+    cout << "State 1:" << endl;
+    for (auto i : items['Z'])
+    {
+        for (auto j : i.second)
+            cout << i.first << " --> " << j << endl;
     }
 
-    
-  
-    unordered_map<int,unordered_map<char, set<string>>> st;
-
-  
-
+    unordered_map<int, unordered_map<char, set<string>>> st;
 
 
     return 0;
 }
-
-
-
-
-
 
 /*
 SAMPLE INPUT
