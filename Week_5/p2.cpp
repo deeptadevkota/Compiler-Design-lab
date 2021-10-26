13348aefcfc871615bda9b229a5188b189e2f58d
deeptadevkota <deeptadevkota21@gmail.com>
2021-10-26 22:06:05 +0545

    state 0 computed

--- Week_5/p2.cpp
+++ Week_5/p2.cpp
@@ -1,20 +1,20 @@
 /*
-Forming a parsing table for LALR grammar
-1)Build the CLR(1) states 
-2)Merge the states to form LALR(1)
+    Forming a parsing table for LALR grammar
+    1)Build the CLR(1) states 
+    2)Merge the states to form LALR(1)
 
 
-changing the data structure to store the states from
+    changing the data structure to store the states from
 
-unordered_map<char, set<string>>> &st 
-to
-unordered_map<char, set<pair<string,string>>>> &st
---> the second element in the pair corresponds to the lookaheads
-again we would pass the given state and all other states to the compute_states() function
+    unordered_map<char, set<string>>> &st 
+    to
+    unordered_map<char, set<pair<string,string>>>> &st
+    --> the second element in the pair corresponds to the lookaheads
+    again we would pass the given state and all other states to the compute_states() function
 
-the change that would make a difference should be in the compute_closure part
+    the change that would make a difference should be in the compute_closure part
 
-now instead of precomputing items what can be done is, we need to send request each time
+    now instead of precomputing items what can be done is, we need to send request each time
 
 
 */
@@ -33,7 +33,7 @@ void calculateFirst(char c, unordered_map<char, set<string>> &m, vector<string> 
             epsilon[c] = true;
             continue;
         }
-        if (vis[i[0]] == 0)
+        if (vis[i[0]] == 0 && c != i[0])
             calculateFirst(i[0], m, first, vis, epsilon);
 
         bool is_epsilon = epsilon[i[0]];
@@ -85,8 +85,9 @@ string firstForString(string str, vector<bool> &epsilon, vector<string> &first)
 }
 
 //implement this function for today
-void compute_closer(char c, string lookup, unordered_map<char, set<string>> &m, unordered_set<char> &nter, unordered_map<char, unordered_map<string, unordered_map<char, set<pair<string, string>>>>> items, unordered_map<char, unordered_map<string, bool>> &isComputed, vector<bool> &epsilon, vector<string> &first)
+void compute_closer(char c, string lookup, unordered_map<char, set<string>> &m, unordered_set<char> &nter, unordered_map<char, unordered_map<string, unordered_map<char, set<pair<string, string>>>>> &items, unordered_map<char, unordered_map<string, bool>> &isComputed, vector<bool> &epsilon, vector<string> &first)
 {
+    cout << "Debug: " << c << " " << lookup << endl;
     set<pair<string, string>> temp;
     //set up the variable temp
     for (auto i : m[c])
@@ -95,35 +96,38 @@ void compute_closer(char c, string lookup, unordered_map<char, set<string>> &m, 
     }
 
     items[c][lookup][c] = temp;
-    
+
     for (auto j : m[c])
     {
 
         //j[1] can be further closured
         if (nter.find(j[1]) != nter.end())
         {
-            string new_str=j.substr(2,j.size());
-            string new_lookup="";
-            for(int i:lookup)
+
+            string new_lookup = "";
+            string new_str = "";
+            if (j.size() >= 3)
+                new_str = j.substr(2, j.size());
+
+            for (int i : lookup)
             {
-                string temp=new_str;
+                string temp = new_str;
                 temp.push_back(i);
-                new_lookup+=firstForString(temp, epsilon, first);
+                new_lookup += firstForString(temp, epsilon, first);
             }
 
-            
-            if(isComputed[j[1]][new_lookup]==false)
+            if (isComputed[j[1]][new_lookup] == false)
             {
                 //for the subsequent recursive call the lookup variable would change
-                compute_closer(j[1], new_lookup, m , nter, items, isComputed, epsilon,first);
+                compute_closer(j[1], new_lookup, m, nter, items, isComputed, epsilon, first);
             }
             for (auto k : items[j[1]][new_lookup])
             {
-                items[c][lookup][k.first]=k.second;
+                items[c][lookup][k.first] = k.second;
             }
         }
     }
-    
+
     isComputed[c][lookup] = true;
 }
 
@@ -148,7 +152,7 @@ int main()
         first[c] = c;
         vis[c] = 1;
     }
-
+    first['$'] = "$";
     cout << "Enter the number  of non-terminals" << endl;
     cin >> n_nter;
 
@@ -180,10 +184,10 @@ int main()
 
     cout << endl
          << endl;
-    cout << "The augmented grammar is:" << endl;
+    // cout << "The augmented grammar is:" << endl;
 
-    for (int i = 0; i <= n_pro; i++)
-        cout << grammar[i] << endl;
+    // for (int i = 0; i <= n_pro; i++)
+    //     cout << grammar[i] << endl;
 
     unordered_map<char, set<string>> m;
 
@@ -211,5 +215,51 @@ int main()
             calculateFirst(grammar[i][0], m, first, vis, epsilon);
         }
     }
+    unordered_map<char, unordered_map<string, unordered_map<char, set<pair<string, string>>>>> items;
+    unordered_map<char, unordered_map<string, bool>> isComputed;
 
+    compute_closer('Z', "$", m, nter, items, isComputed, epsilon, first);
+
+    cout << endl
+         << endl;
+    cout << "State: 0" << endl
+         << endl;
+
+    //basically printing the closure of Z on lookup '$'
+
+    for (auto i : items['Z']["$"])
+    {
+        for (auto j : i.second)
+        {
+            cout << i.first << " --> " << j.first << " , ";
+            for (int k = 0; k < j.second.size() - 1; k++)
+            {
+                cout << j.second[k] << "|";
+            }
+            cout << j.second[j.second.size() - 1] << endl;
+        }
+    }
+
+    cout << "COMPUTED" << endl;
+    return 0;
 }
+
+/*
+
+
+
+3
+a
+b
+c
+2
+S
+A
+2
+S=A
+A=aAb|c
+S
+
+
+
+*/
